234f23b225f43bb68b9a9e5af328fa5fd8948690
nilesh146 <nileshkumarjaiswal88@gmail.com>
2020-02-08 19:13:31 +0530

    merge conflict

--- src/main/java/com/ace/mainfinance/MainFin.java
+++ src/main/java/com/ace/mainfinance/MainFin.java
@@ -5,6 +5,7 @@ public class MainFin {
 	public static void main(String[] args) {
 		
 		System.out.println("not before");
+		System.out.println("before main fin started");
 		System.out.println("main fin started");
 	}
 
